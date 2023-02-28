cff75809c81f8af8410845a54d4d7bfc42a61459
themuuln <zerone.offical@gmail.com>
2023-02-28 15:59:05 +0800

    ui: scrollbar & scrollY added

--- components/header/HeaderSection.tsx
+++ components/header/HeaderSection.tsx
@@ -1,10 +1,27 @@
+"use client";
+import { motion } from "framer-motion";
+
 const HeaderSection = () => {
+  const cardVariants = {
+    offscreen: {
+      y: 300,
+    },
+    onscreen: {
+      y: 50,
+      rotate: -10,
+      transition: {
+        type: "spring",
+        bounce: 0.4,
+        duration: 0.8,
+      },
+    },
+  };
   return (
     <>
       <div className="h-[80px] w-full backdrop-blur">
         <div className="flex justify-between">
           <div className="leftSideHeader">test1</div>
-          <div className="flex gap-1 rightSideHeader">
+          <div className="rightSideHeader flex gap-2 font-bold text-brand_main-500">
             <div>WHY ME?</div>
             <div>PROJECTS</div>
           </div>
--- components/hero/HeroSection.tsx
+++ components/hero/HeroSection.tsx
@@ -1,26 +1,55 @@
 "use client";
 import { HiOutlineChevronDoubleDown } from "react-icons/hi";
 import { IconContext } from "react-icons";
+import { motion, useScroll, useSpring } from "framer-motion";
 
 function HeroSection() {
+  const { scrollYProgress } = useScroll();
+  const scaleX = useSpring(scrollYProgress, {
+    stiffness: 100,
+    damping: 30,
+    restDelta: 0.001,
+  });
   return (
     <>
+      <motion.div
+        className="progress-bar fixed top-0 left-0 right-0 h-[10px]  bg-brand_main-500"
+        style={{ scaleX }}
+      />
       <IconContext.Provider value={{ size: "3em" }}>
         <main
           className={
             " flex h-screen w-screen  items-center justify-center bg-brand_bg-500 text-brand_main-500"
           }
         >
-          <div className="container flex flex-col items-center justify-center h-full">
-            <div className="flex flex-col justify-start select-none textContainer text-7xl">
+          <motion.div className="container flex h-full flex-col items-center justify-center">
+            <div className="textContainer flex select-none flex-col justify-start text-7xl md:text-[16px]">
               {/* <p>Hi, I&apos;m</p> */}
-              <p className="text-3xl">Hello, my name is</p>
-              <p className="text-[100px] font-bold">Temuulen Undrakhbayar</p>
-              <p className="text-3xl">Front-end Developer & UI/UX Designer</p>
+              <motion.p className="text-3xl">Hello, my name is</motion.p>
+              <motion.p
+                initial={{ x: 100, y: 100, opacity: 0 }}
+                animate={{ x: 0, y: 0, opacity: 1 }}
+                transition={{
+                  type: "spring",
+                  stiffness: 100,
+                  duration: 2,
+                }}
+                className="text-[100px] font-bold "
+              >
+                TEMUULEN UNDRAKHBAYAR
+              </motion.p>
+              <div className="h-40 w-full bg-brand_bg-500">
+                {/* <motion.p
+                  initial={{ x: -100, opacity: 0 }}
+                  animate={{ x: 0, opacity: 1 }}
+                  className="text-3xl "
+                >
+                  Front-end Developer & UI/UX Designer
+                </motion.p> */}
+              </div>
             </div>
-          </div>
-
-          <HiOutlineChevronDoubleDown className="absolute select-none bottom-4 animate-bounce" />
+          </motion.div>
+          <HiOutlineChevronDoubleDown className="absolute bottom-4 animate-bounce select-none" />
         </main>
       </IconContext.Provider>
     </>
