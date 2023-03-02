7b8f177b9f5185beb4a1b108c98a95593f3285d9
themuuln <zerone.offical@gmail.com>
2023-03-02 17:13:00 +0800

    update: header blog button added

--- components/header/HeaderRight.tsx
+++ components/header/HeaderRight.tsx
@@ -3,10 +3,11 @@ import Buttons from "./Buttons";
 const HeaderRight = () => {
   return (
     <>
-      <div className="flex gap-5 text-2xl font-bold rightSideHeader text-brand_main-500">
+      <div className="rightSideHeader flex gap-5 text-2xl font-bold text-brand_main-500">
         <Buttons context={"home"} />
         <Buttons context={"experience"} />
         <Buttons context={"work"} />
+        <Buttons context={"blog"} />
         <Buttons context={"contact"} />
       </div>
     </>
