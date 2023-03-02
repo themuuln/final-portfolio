245cf5bad0c7e59ef4d25ddc192b771c3e76958c
themuuln <zerone.offical@gmail.com>
2023-03-02 17:09:00 +0800

    ui: header button upgrade

--- components/header/Buttons.tsx
+++ components/header/Buttons.tsx
@@ -1,4 +1,5 @@
 import { Fira_Code } from "@next/font/google";
+import { motion } from "framer-motion";
 
 const firacode = Fira_Code({
   subsets: ["latin"],
@@ -12,11 +13,16 @@ type Props = {
 const Buttons = ({ context }: Props) => {
   return (
     <>
-      <div
+      <motion.div
+        whileHover={{ scale: 1.2 }}
+        whileTap={{ scale: 0.9 }}
+        transition={{ type: "spring", stiffness: 400, damping: 10 }}
+        initial={{ x: 100, opacity: 0 }}
+        whileInView={{ x: 0, opacity: 1, y: 0 }}
         className={`${firacode.className} cursor-pointer font-sans transition ease-linear hover:text-brand_main-300`}
       >
         \\ {context}
-      </div>
+      </motion.div>
     </>
   );
 };
