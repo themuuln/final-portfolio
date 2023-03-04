f7bde5abce435924966df18059735ed06e50782b
themuuln <zerone.offical@gmail.com>
2023-03-04 20:00:00 +0800

    update: link added

--- components/header/HeaderRight.tsx
+++ components/header/HeaderRight.tsx
@@ -1,14 +1,25 @@
 import Buttons from "./Buttons";
+import Link from "next/link";
 
 const HeaderRight = () => {
   return (
     <>
-      <div className="rightSideHeader flex gap-5 text-2xl font-bold text-brand_main-500">
-        <Buttons context={"home"} />
-        <Buttons context={"experience"} />
-        <Buttons context={"work"} />
-        <Buttons context={"blog"} />
-        <Buttons context={"contact"} />
+      <div className="flex gap-5 text-2xl font-bold rightSideHeader text-brand_main-500">
+        <Link href={"#home"}>
+          <Buttons context={"home"} />
+        </Link>
+        <Link href={"/experience"}>
+          <Buttons context={"experience"} />
+        </Link>
+        <Link href={"/work"}>
+          <Buttons context={"work"} />
+        </Link>
+        <Link href={"/blog"}>
+          <Buttons context={"blog"} />
+        </Link>
+        <Link href={"/contact"}>
+          <Buttons context={"contact"} />
+        </Link>
       </div>
     </>
   );
