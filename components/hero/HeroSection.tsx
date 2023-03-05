664d1319961aee782c8d3404dfe3b5e62eccaf6d
themuuln <zerone.offical@gmail.com>
2023-03-05 13:29:17 +0800

    husky test

--- components/hero/HeroSection.tsx
+++ components/hero/HeroSection.tsx
@@ -4,20 +4,25 @@ import ScrollDownAnimation from "./ScrollDownAnimation";
 import Role from "./Role";
 import HeroMyName from "./HeroMyName";
 import HelloGreetings from "./HeroGreetings";
+import { Montserrat } from "@next/font/google";
+
+const montserrat = Montserrat({
+  subsets: ["latin"],
+  weight: ["300", "400", "500", "600", "700", "800", "900"],
+  variable: "--font-montserrat",
+});
 
 function HeroSection() {
   return (
     <>
       <main
-        className={
-          "flex min-h-screen w-screen items-center justify-center bg-brand_bg-500 text-brand_main-500"
-        }
+        className={`${montserrat.className} flex min-h-screen w-screen items-center justify-center bg-brand_bg-500 text-brand_main-500`}
       >
         <motion.div className="container flex h-full flex-col items-center justify-center">
           <div className="textContainer flex select-none flex-col justify-start text-7xl">
             <HelloGreetings greetings={"Hello, my name is"} />
             <HeroMyName myName={"temuulen undrakhbayar"} />
-            <Role role={"Front-end Developer & UI/UX Designer"} />
+            <Role role={"Front-end Developer & UI Designer"} />
           </div>
         </motion.div>
         <ScrollDownAnimation />
