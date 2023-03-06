d35826ad57657c4180e6b47a540e752840c33d23
themuuln <zerone.offical@gmail.com>
2023-03-06 12:10:55 +0800

    ui: button hover onclick effect changed

--- components/header/Buttons.tsx
+++ components/header/Buttons.tsx
@@ -14,8 +14,8 @@ const Buttons = ({ context }: Props) => {
   return (
     <>
       <motion.div
-        whileHover={{ scale: 1.2 }}
-        whileTap={{ scale: 0.9 }}
+        whileHover={{ y: -10 }}
+        whileTap={{ color: "#cc6a27" }}
         transition={{ type: "spring", stiffness: 400, damping: 10 }}
         initial={{ x: 100, opacity: 0 }}
         whileInView={{ x: 0, opacity: 1, y: 0 }}
