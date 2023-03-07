ecf5b16494d39e64b93c923247f69b68e564cbdd
themuuln <zerone.offical@gmail.com>
2023-03-07 12:02:54 +0800

    ui: footer color adjusted

--- components/footer/FooterSection.tsx
+++ components/footer/FooterSection.tsx
@@ -2,13 +2,13 @@ const FooterSection = () => {
   return (
     <>
       <section className="relative h-[20vh] ">
-        <div className="container relative flex h-full w-[90vw] flex-col items-center gap-2 rounded-lg bg-brand_bg-300">
+        <div className="container relative flex h-full w-[90vw] flex-col items-center gap-2 rounded-lg bg-brand_bg-700">
           <h2>Start a project</h2>
           <p>Work together?</p>
           <button>Go ahead!</button>
         </div>
       </section>
-      <footer className=" bg-brand_bg-400">
+      <footer className=" bg-brand_bg-600">
         <div className="container h-[40vh]">Footer Section</div>
       </footer>
     </>
