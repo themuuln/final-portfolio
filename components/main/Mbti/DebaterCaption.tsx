0372bcab170ed0b86fc941690cd1f3f3548df6ce
themuuln <zerone.offical@gmail.com>
2023-03-07 12:01:12 +0800

    change: debater entp context text to svg changed

--- components/main/Mbti/DebaterCaption.tsx
+++ components/main/Mbti/DebaterCaption.tsx
@@ -1,13 +1,15 @@
 import { poppins, montserrat } from "@/public/fonts/fonts";
+import DebaterText from "@/public/svg/DebaterText";
 
 const DebaterCaption = () => {
   return (
     <>
-      <h2
+      {/* <h2
         className={`${poppins.className} text-6xl font-semibold text-brand_main-500`}
       >
         Debater
-      </h2>
+      </h2> */}
+      <DebaterText />
       <h3
         className={`${montserrat.className} text text-2xl text-brand_main-400 underline underline-offset-1`}
       >
