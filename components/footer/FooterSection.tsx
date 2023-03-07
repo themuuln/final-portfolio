9e397b10ffd17bad63873c66dddaf572239465d5
themuuln <zerone.offical@gmail.com>
2023-03-07 12:00:38 +0800

    added: footer section main template context added

--- components/footer/FooterSection.tsx
+++ components/footer/FooterSection.tsx
@@ -1,7 +1,16 @@
 const FooterSection = () => {
   return (
     <>
-      <div className="h-[60vh] bg-brand_bg-400">Footer Section</div>
+      <section className="relative h-[20vh] ">
+        <div className="container relative flex h-full w-[90vw] flex-col items-center gap-2 rounded-lg bg-brand_bg-300">
+          <h2>Start a project</h2>
+          <p>Work together?</p>
+          <button>Go ahead!</button>
+        </div>
+      </section>
+      <footer className=" bg-brand_bg-400">
+        <div className="container h-[40vh]">Footer Section</div>
+      </footer>
     </>
   );
 };
