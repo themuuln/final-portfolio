ead1c2a47a352b89f911b139e6e2adf49a677ea5
themuuln <zerone.offical@gmail.com>
2023-03-07 12:03:24 +0800

    optimize: mbtiSection cleaned up

--- components/main/Mbti/MbtiSection.tsx
+++ components/main/Mbti/MbtiSection.tsx
@@ -1,20 +1,13 @@
 "use client";
-import DebaterSvg from "@/public/svg/DebaterSvg";
-import ChartLeft from "./ChartLeft";
-import ChartPercentage from "./ChartPercentage";
-import ChartRight from "./ChartRight";
-import DebaterCaption from "./DebaterCaption";
 import EntpAvatar from "./EntpAvatar";
 import EntpStats from "./EntpStats";
 
 const MbtiSection = () => {
   return (
     <>
-      <div>
-        <div className="container flex h-screen flex-col items-center justify-center gap-8 lg:flex-row xl:gap-20">
-          <EntpAvatar />
-          <EntpStats />
-        </div>
+      <div className="container flex h-screen flex-col items-center justify-center gap-8 lg:flex-row xl:gap-20">
+        <EntpAvatar />
+        <EntpStats />
       </div>
     </>
   );
