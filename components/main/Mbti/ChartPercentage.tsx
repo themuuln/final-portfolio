45d2cf56b9be38d5d72701d088479962df80dfca
themuuln <zerone.offical@gmail.com>
2023-03-07 11:55:36 +0800

    ui: stats percentage weight fixed

--- components/main/Mbti/ChartPercentage.tsx
+++ components/main/Mbti/ChartPercentage.tsx
@@ -1,18 +1,43 @@
-import { montserrat } from "@/public/fonts/fonts";
+"use client";
+import { motion as m } from "framer-motion";
+import { montserrat, kanit } from "@/public/fonts/fonts";
 
 type Props = {
   left: String;
   right: String;
   leftColor: String;
   rightColor: String;
+  leftContext: String;
+  rightContext: String;
 };
-const ChartPercentage = ({ left, right, leftColor, rightColor }: Props) => {
+const ChartPercentage = ({
+  left,
+  right,
+  leftColor,
+  rightColor,
+  leftContext,
+  rightContext,
+}: Props) => {
   return (
     <>
-      <div className="mt-[-10px] flex h-fit w-full justify-between px-3 text-lg">
-        <p className={`${montserrat.className} ${leftColor}`}>{left}</p>
-        <p className={`${montserrat.className} ${rightColor}`}>{right}</p>
-      </div>
+      <m.div
+        initial={{ x: 1000, opacity: 0 }}
+        whileInView={{ x: 0, opacity: 1 }}
+        className="mt-[-10px] flex h-fit w-full justify-between px-3 text-lg"
+      >
+        <div className="flex gap-2">
+          <p className={`${montserrat.className} ${leftColor}`}>{left}</p>
+          <p className={`${kanit.className} ${leftColor}`}>
+            {leftContext ? leftContext.toUpperCase() : ""}
+          </p>
+        </div>
+        <div className="flex gap-2">
+          <p className={`${kanit.className} ${rightColor}`}>
+            {rightContext ? leftContext.toUpperCase() : ""}
+          </p>
+          <p className={`${montserrat.className} ${rightColor}`}>{right}</p>
+        </div>
+      </m.div>
     </>
   );
 };
