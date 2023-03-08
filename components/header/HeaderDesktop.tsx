959170f46f1f5a1b8af517bde482476909a5b1ec
themuuln <zerone.offical@gmail.com>
2023-03-08 15:29:49 +0800

    bug: header link paths fixed

--- components/header/HeaderDesktop.tsx
+++ components/header/HeaderDesktop.tsx
@@ -8,16 +8,16 @@ const HeaderDesktop = () => {
         <Link href={"#home"}>
           <Buttons context={"home"} />
         </Link>
-        <Link href={"/experience"}>
+        <Link href={"#experience"}>
           <Buttons context={"experience"} />
         </Link>
-        <Link href={"/project"}>
+        <Link href={"#project"}>
           <Buttons context={"project"} />
         </Link>
-        <Link href={"/blog"}>
+        <Link href={"#blog"}>
           <Buttons context={"blog"} />
         </Link>
-        <Link href={"/contact"}>
+        <Link href={"#contact"}>
           <Buttons context={"contact"} />
         </Link>
       </div>
