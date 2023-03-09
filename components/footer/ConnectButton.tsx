a24a80584c2ca010a2eacdbf98ca88ed8b2d298b
themuuln <zerone.offical@gmail.com>
2023-03-09 17:07:35 +0800

    ui: footer social link button hover effect fixed

--- components/footer/ConnectButton.tsx
+++ components/footer/ConnectButton.tsx
@@ -1,7 +1,7 @@
 const ConnectButton = ({ icon, href }: any) => {
   return (
     <a
-      className="rounded-full border-2 border-brand_main-300 p-2 transition-all hover:border-brand_main-700 hover:bg-brand_main-500 hover:text-brand_main-600"
+      className="rounded-full border-[1px] border-brand_main-400 p-2 transition-all hover:border-brand_main-500 hover:bg-brand_main-500 hover:text-brand_bg-500"
       href={href}
       target={"blank"}
     >
