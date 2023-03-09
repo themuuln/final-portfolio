57ecf2b8ac4dfcfdc96348da0f8ebbd76d2737c1
themuuln <zerone.offical@gmail.com>
2023-03-09 16:20:39 +0800

    ui: headerleft whileinview fixed

--- components/header/HeaderLeft.tsx
+++ components/header/HeaderLeft.tsx
@@ -9,11 +9,11 @@ const HeaderLeft = ({ context }: Props) => {
   return (
     <>
       <m.div
-        initial={{ x: -200, opacity: 0 }}
-        animate={{ x: 0, opacity: 1 }}
-        className="leftSideHeader flex cursor-pointer items-center gap-1 text-xl font-bold text-brand_main-500 hover:text-brand_main-600"
+        initial={{ x: 200, opacity: 0 }}
+        whileInView={{ x: 0, opacity: 1 }}
+        className="leftSideHeader flex cursor-pointer justify-center gap-1 text-xl font-bold text-brand_main-500 hover:text-brand_main-600"
       >
-        <IconContext.Provider value={{ size: "2em" }}>
+        <IconContext.Provider value={{ size: "1.5em" }}>
           <AiOutlineCode />
         </IconContext.Provider>
         {context}
