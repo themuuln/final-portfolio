e15d6d8a32b5b2072fff74b7beb83b72a9456591
themuuln <zerone.offical@gmail.com>
2023-03-09 17:07:15 +0800

    fix: website description fixed

--- app/head.tsx
+++ app/head.tsx
@@ -5,7 +5,7 @@ export default function Head() {
       <meta content="width=device-width, initial-scale=1" name="viewport" />
       <meta
         name="description"
-        content="Welcome to my portfolio website, where I showcase my expertise in building modern web applications using the latest front-end technologies. With extensive experience in React, Next.js, TypeScript, Framer Motion, Tailwind CSS, and Chakra UI, I am skilled in developing responsive, performant, and visually stunning interfaces. Whether it's designing a user-friendly UI, implementing complex animations, or optimizing the codebase for maximum efficiency, I have the knowledge and creativity to deliver outstanding results. Please feel free to explore my projects and learn more about my skills and experience."
+        content="Welcome to my portfolio website, where I showcase my expertise in building modern web applications using the latest front-end technologies. With extensive experience in React, Next.js, TypeScript, Framer Motion, Tailwind CSS, I am skilled in developing responsive, performant, and visually stunning interfaces. Whether it's designing a user-friendly UI, implementing complex animations, or optimizing the codebase for maximum efficiency, I have the knowledge and creativity to deliver outstanding results. Please feel free to explore my projects and learn more about my skills and experience."
       />
       <link rel="icon" href="/favicon.ico" />
     </>
