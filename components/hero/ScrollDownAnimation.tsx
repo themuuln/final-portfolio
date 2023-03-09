af214e4ffb5466b1d4acccb191dc27a7f4d46e76
themuuln <zerone.offical@gmail.com>
2023-03-09 13:10:15 +0800

    optimize: context removed

--- components/hero/ScrollDownAnimation.tsx
+++ components/hero/ScrollDownAnimation.tsx
@@ -1,15 +1,12 @@
 import { motion } from "framer-motion";
-import { IconContext } from "react-icons";
 import ScrollDown from "@/public/svg/ScrollDown";
 
 const ScrollDownAnimation = () => {
   return (
     <>
-      <IconContext.Provider value={{ size: "3.5em" }}>
-        <motion.div initial={{ opacity: 0 }} whileInView={{ opacity: 1 }}>
-          <ScrollDown />
-        </motion.div>
-      </IconContext.Provider>
+      <motion.div initial={{ opacity: 0 }} whileInView={{ opacity: 1 }}>
+        <ScrollDown />
+      </motion.div>
     </>
   );
 };
