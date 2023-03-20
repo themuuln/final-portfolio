9361e81dd3d713c00d5c4eaaba57c094219244aa
themuuln <zerone.offical@gmail.com>
2023-03-20 11:47:32 +0800

    fix: header responsive bug fixed

--- components/header/HeaderRight.tsx
+++ components/header/HeaderRight.tsx
@@ -1,18 +1,18 @@
 import dynamic from "next/dynamic";
 import { useMediaQuery } from "react-responsive";
 
-const HeaderMobile = dynamic(() => import("./HeaderMobile"));
-const HeaderDesktop = dynamic(() => import("./HeaderDesktop"));
+const HeaderMobile = dynamic(() => import("./HeaderMobile"), { ssr: false });
+const HeaderDesktop = dynamic(() => import("./HeaderDesktop"), { ssr: false });
 
 const HeaderRight = () => {
   // return <HeaderDesktop />;
-  const isDesktopOrLaptop = useMediaQuery({
-    query: "(min-width: 1224px)",
-  });
-  const isBigScreen = useMediaQuery({ query: "(min-width: 1824px)" });
-  const isTabletOrMobile = useMediaQuery({ query: "(max-width: 1224px)" });
+  // const isDesktopOrLaptop = useMediaQuery({
+  //   query: "(min-width: 1224px)",
+  // });
+  // const isBigScreen = useMediaQuery({ query: "(min-width: 1824px)" });
+  // const isTabletOrMobile = useMediaQuery({ query: "(max-width: 1224px)" });
   const isPortrait = useMediaQuery({ query: "(orientation: portrait)" });
-  const isRetina = useMediaQuery({ query: "(min-resolution: 2dppx)" });
+  // const isRetina = useMediaQuery({ query: "(min-resolution: 2dppx)" });
 
   return isPortrait ? <HeaderMobile /> : <HeaderDesktop />;
 };
