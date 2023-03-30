1d55a46d9df864153a831c81b6e087c5bebc1dbd
themuuln <zerone.offical@gmail.com>
2023-03-30 16:06:31 +0800

    update: footer name

--- components/footer/FooterGit.tsx
+++ components/footer/FooterGit.tsx
@@ -18,7 +18,9 @@ const FooterGit = () => {
     <>
       <div className={`${firacode.className} mb-10`}>
         <div className={`flex w-full justify-center`}>
-          <p>Designed & Built by Themuuln</p>
+          <p>
+            Designed & Built by <span className="">Themuuln</span>
+          </p>
         </div>
         {/* <div className={`mt-3 flex w-full justify-center `}>
           <p>
