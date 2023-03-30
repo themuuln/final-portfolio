8427aba5c4ecdd72f5e69b71585e14db5add62bd
themuuln <zerone.offical@gmail.com>
2023-03-30 16:09:01 +0800

    not done

---
+++ components/main/skills/SkillsList.tsx
@@ -0,0 +1,35 @@
+import { motion as m } from "framer-motion";
+import CursorContext from "@/lib/context/context";
+import { HoverTypeContext } from "@/lib/context/HoverTypeContext";
+import { HiOutlineCode } from "react-icons/hi";
+import { skills } from "@/pages/api/skills";
+
+import { useContext } from "react";
+
+const SkillsList = (skill: any) => {
+  const { hoverType, setHoverType } = useContext(HoverTypeContext);
+  const { textEnter, textLeave } = useContext(CursorContext);
+
+  const handleMouseEnter = () => {
+    textEnter();
+    // setHoverType(skills[0].languages[0].icon);
+    setHoverType(<HiOutlineCode />);
+  };
+  return (
+    <>
+      <m.li
+        whileHover={{ x: 40, scale: 1.1 }}
+        onMouseEnter={handleMouseEnter}
+        onMouseLeave={textLeave}
+        className={"transition-colors duration-200 hover:text-brand_bg-200"}
+        key={skill.name}
+      >
+        <m.a href={skill.href} target="_blank" rel="noopener noreferrer">
+          {skill.name}
+        </m.a>
+      </m.li>
+    </>
+  );
+};
+
+export default SkillsList;
