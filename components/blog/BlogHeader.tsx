24e54ae647f882c1ba912c18eba4062f1e13891a
themuuln <zerone.offical@gmail.com>
2023-03-30 16:05:21 +0800

    update: blog header icon context updated

--- components/blog/BlogHeader.tsx
+++ components/blog/BlogHeader.tsx
@@ -3,6 +3,10 @@ import HeaderLeft from "../header/HeaderLeft";
 import Link from "next/link";
 import Buttons from "../header/Buttons";
 import { firacode } from "@/public/fonts/fonts";
+import { AiOutlineExperiment } from "react-icons/ai";
+import { RiContactsBook2Line } from "react-icons/ri";
+import { BsFileEarmarkPost } from "react-icons/bs";
+import { HiOutlineHome, HiTemplate } from "react-icons/hi";
 
 const BlogHeader = () => {
   return (
@@ -13,19 +17,19 @@ const BlogHeader = () => {
         <HeaderLeft context={"themuln.official@gmail.com"} />
         <div className="rightSideHeader flex gap-5 text-2xl font-bold ">
           <Link href={"#home"}>
-            <Buttons context={"home"} />
+            <Buttons icon={<HiOutlineHome />} context={"home"} />
           </Link>
           <Link href={"#experience"}>
-            <Buttons context={"experience"} />
+            <Buttons icon={<AiOutlineExperiment />} context={"experience"} />
           </Link>
           <Link href={"#project"}>
-            <Buttons context={"project"} />
+            <Buttons icon={<HiTemplate />} context={"project"} />
           </Link>
           <Link href={"/blog"}>
-            <Buttons context={"blog"} />
+            <Buttons icon={<BsFileEarmarkPost />} context={"blog"} />
           </Link>
           <Link href={"#contact"}>
-            <Buttons context={"contact"} />
+            <Buttons icon={<RiContactsBook2Line />} context={"contact"} />
           </Link>
         </div>
       </div>
