0fa47468bf8fbdbae67504387492a61f0c3392d2
themuuln <zerone.offical@gmail.com>
2023-04-02 23:02:49 +0800

    removed: useless grid removed

--- components/main/MainSection.tsx
+++ components/main/MainSection.tsx
@@ -7,13 +7,6 @@ const MainSection = () => {
     <>
       <Qna />
       <Skills />
-      <div className="container">
-        <div className="mx-10 grid h-screen grid-rows-2 gap-10 py-10 md:grid-cols-2 ">
-          <div className="informationCard col-start-1 col-end-3">1</div>
-          <div className="informationCard col-start-1 col-end-2">2</div>
-          <div className="informationCard col-start-2 col-end-3">3</div>
-        </div>
-      </div>
     </>
   );
 };
