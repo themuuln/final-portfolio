7bf492795e26ec9354e136d3a6d7668aba24bc88
themuuln <zerone.offical@gmail.com>
2023-04-02 22:59:54 +0800

    ui: footer height adjusted

--- components/footer/FooterSection.tsx
+++ components/footer/FooterSection.tsx
@@ -1,21 +1,17 @@
 "use client";
 import { IconContext } from "react-icons";
 import FooterMain from "./FooterMain";
-import FooterStartSection from "./FooterStartSection";
 
 const FooterSection = () => {
   return (
-    <div
-      id="footer"
-      className="flex flex-col justify-end pt-20 h-fit snap-center"
-    >
+    <section id="footer" className="flex h-fit flex-col justify-end pt-20">
       <footer className="text-brand_main-500 bg-brand_bg-600/5">
         <IconContext.Provider value={{ size: "1em" }}>
-          <FooterStartSection />
+          {/* <FooterStartSection /> */}
           <FooterMain />
         </IconContext.Provider>
       </footer>
-    </div>
+    </section>
   );
 };
 
