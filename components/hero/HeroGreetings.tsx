c7f4cdcb183504117932922b05f7152b22e9c5cf
themuuln <zerone.offical@gmail.com>
2023-04-02 15:28:25 +0800

    ui: animation changed, text size responsive, clean

--- components/hero/HeroGreetings.tsx
+++ components/hero/HeroGreetings.tsx
@@ -1,21 +1,18 @@
 import { motion as m } from "framer-motion";
 import { firacode } from "@/public/fonts/fonts";
-import { useContext } from "react";
-import CursorContext from "@/lib/context/context";
 
 const HelloGreetings = ({ greetings }: { greetings: String }) => {
-  const { textEnter, textLeave } = useContext(CursorContext);
   return (
     <>
       <m.p
-        initial={{ x: -100, opacity: 0 }}
-        whileInView={{ x: 0, opacity: 1 }}
+        initial={{ x: -100, y: -100, opacity: 0 }}
+        animate={{ x: 0, y: 0, opacity: 1 }}
         transition={{
-          type: "spring",
-          stiffness: 100,
-          duration: 1,
+          // type: "spring",
+          // stiffness: 100,
+          duration: 0.5,
         }}
-        className={`${firacode.className} mb-2 w-fit text-xl font-light leading-4 xl:text-3xl xl:leading-6`}
+        className={`${firacode.className} mb-2 w-fit text-lg font-light leading-3 sm:text-xl md:text-2xl md:leading-3 xl:text-3xl xl:leading-6`}
       >
         {greetings.split("").map((letter, index) => (
           <m.span
