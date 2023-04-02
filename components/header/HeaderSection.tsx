603328a981ea38be9f8c9a6417c03460e723c859
themuuln <zerone.offical@gmail.com>
2023-04-02 23:03:09 +0800

    changed: header section added

--- components/header/HeaderSection.tsx
+++ components/header/HeaderSection.tsx
@@ -5,12 +5,12 @@ import HeaderRight from "./HeaderRight";
 const HeaderSection = () => {
   return (
     <>
-      <div className="text-white sticky top-0 left-0 z-10 h-[80px] w-full py-6 px-8 backdrop-blur-sm">
+      <header className="sticky top-0 left-0 z-10 h-[80px] w-full py-6 px-8 text-white backdrop-blur-sm">
         <div className="container flex justify-between ">
           <HeaderLeft context={"themuln.official@gmail.com"} />
           <HeaderRight />
         </div>
-      </div>
+      </header>
     </>
   );
 };
