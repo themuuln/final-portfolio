cd591a2e0297f4dbaf762ccaad28fe9e0fe322bf
themuuln <zerone.offical@gmail.com>
2023-04-02 16:27:42 +0800

    cleaned up

--- components/hero/HeroSection.tsx
+++ components/hero/HeroSection.tsx
@@ -1,5 +1,4 @@
 "use client";
-import { motion } from "framer-motion";
 import Role from "./Role";
 import HeroMyName from "./HeroMyName";
 import HelloGreetings from "./HeroGreetings";
@@ -10,15 +9,15 @@ function HeroSection() {
     <>
       <main
         id="hero"
-        className={`${telegraf_ultrabold.className} text-white flex min-h-screen w-screen snap-center items-center justify-center `}
+        className={`flex min-h-screen w-screen snap-center items-center justify-center text-white `}
       >
-        <motion.div className="container flex h-full w-screen flex-col items-center justify-center">
+        <div className="container flex h-full w-screen flex-col items-center justify-center">
           <div className="textContainer flex select-none flex-col justify-start">
             <HelloGreetings greetings={"Hello, my name is"} />
             <HeroMyName myName={"temuulen undrakhbayar"} />
             <Role role={"Front-end Developer & UI Designer"} />
           </div>
-        </motion.div>
+        </div>
       </main>
     </>
   );
