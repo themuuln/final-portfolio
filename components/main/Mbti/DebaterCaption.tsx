f35e0d384e522f8411973fd82102b265b40bbd2b
themuuln <zerone.offical@gmail.com>
2023-04-02 23:00:42 +0800

    ui: animation changed

--- components/main/Mbti/DebaterCaption.tsx
+++ components/main/Mbti/DebaterCaption.tsx
@@ -23,10 +23,11 @@ const DebaterCaption = () => {
       <m.a
         href="https://www.16personalities.com/profiles/aadde637ba318"
         target={"_blank"}
-        initial={{ x: -100, opacity: 0 }}
+        initial={{ x: -100, y: -100, opacity: 0 }}
+        whileInView={{ x: 0, y: 0, opacity: 1 }}
+        transition={{ duration: 0.3 }}
         onMouseEnter={handleMouseEnter}
         onMouseLeave={handleMouseLeave}
-        whileInView={{ x: 0, opacity: 1 }}
         className={`${poppins.className} text-brand_main-500 text-6xl font-semibold transition-colors duration-300 hover:text-brand_bg-500`}
       >
         Debater
@@ -34,10 +35,11 @@ const DebaterCaption = () => {
       <m.a
         href="https://www.16personalities.com/profiles/aadde637ba318"
         target={"_blank"}
-        initial={{ x: -100, opacity: 0 }}
+        initial={{ x: -100, y: 100, opacity: 0 }}
+        whileInView={{ x: 0, y: 0, opacity: 1 }}
+        transition={{ duration: 0.3 }}
         onMouseEnter={handleMouseEnter}
         onMouseLeave={handleMouseLeave}
-        whileInView={{ x: 0, opacity: 1 }}
         className={`${montserrat.className} text text-brand_main-400 text-2xl underline underline-offset-1`}
       >
         ENTP-T
