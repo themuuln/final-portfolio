2d011acdeac6d01f0640a9d4012b206bc1d2c3b5
themuuln <zerone.offical@gmail.com>
2023-04-02 16:27:28 +0800

    ui: font weight fixed

--- components/header/HeaderMobile.tsx
+++ components/header/HeaderMobile.tsx
@@ -3,7 +3,7 @@ import { firacode } from "@/public/fonts/fonts";
 const HeaderMobile = () => {
   return (
     <>
-      <div className={`${firacode.className} cursor-pointer`}>
+      <div className={`${firacode.className} cursor-pointer font-light`}>
         (under construction)
       </div>
     </>
