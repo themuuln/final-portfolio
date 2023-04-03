bcf567612f51b813f6ffde209ff9c9237db2c015
themuuln <zerone.offical@gmail.com>
2023-04-03 19:18:28 +0800

    update: blog header ... updated

--- app/blog/layout.tsx
+++ app/blog/layout.tsx
@@ -13,7 +13,6 @@ export default function RootLayout({
     <html lang="en">
       <head />
       <body>
-        <BlogHeader />
         <>{children}</>
         <FooterSection />
       </body>
--- app/blog/page.tsx
+++ app/blog/page.tsx
@@ -1,11 +1,131 @@
+"use client";
 import BlogMainSection from "@/components/blog/BlogMainSection";
 
-const index = () => {
+import { AnimatePresence, motion as m } from "framer-motion";
+import { useState } from "react";
+
+import CursorContext from "@/lib/context/context";
+import useWindowEvents from "@/lib/hook/WindowEvents";
+import { HoverTypeContext } from "@/lib/context/HoverTypeContext";
+import { MixBlendMode } from "@/lib/types/types";
+import { zen_tokyo_zoo } from "@/public/fonts/fonts";
+
+import { FiArrowUpRight } from "react-icons/fi";
+import { IconContext } from "react-icons";
+import HeaderSection from "@/components/header/HeaderSection";
+import BlogHeader from "@/components/blog/BlogHeader";
+
+const Blog = () => {
+  const [hoverType, setHoverType] = useState(<FiArrowUpRight />);
+  const [cursorVariant, setCursorVariant] = useState("default");
+  const { mousePosition } = useWindowEvents();
+
+  const variants = {
+    default: {
+      x: mousePosition.x - 5,
+      y: mousePosition.y - 5,
+    },
+    text: {
+      height: 101,
+      width: 101,
+      x: mousePosition.x - 51,
+      y: mousePosition.y - 51,
+      mixBlendMode: "color-dodge" as MixBlendMode,
+    },
+  };
+
+  const variants3 = {
+    default: {
+      x: mousePosition.x - 17,
+      y: mousePosition.y - 17,
+    },
+    text: {
+      height: 126,
+      width: 126,
+      x: mousePosition.x - 63.5,
+      y: mousePosition.y - 63.5,
+      mixBlendMode: "color-dodge" as MixBlendMode,
+    },
+  };
+
+  const variants4 = {
+    default: {
+      x: mousePosition.x - 3,
+      y: mousePosition.y - 3,
+    },
+    text: {
+      height: 51,
+      width: 81,
+      display: "flex",
+      x: mousePosition.x - 41,
+      y: mousePosition.y - 26,
+    },
+    transition: {
+      type: "spring",
+      damping: 11,
+      stiffness: 51,
+      restDelta: 1.001,
+    },
+    exit: {
+      opacity: 0,
+      transition: {
+        duration: 0.5,
+        ease: "easeInOut",
+      },
+    },
+  };
+
+  const textEnter = () => setCursorVariant("text");
+  const textLeave = () => setCursorVariant("default");
+
   return (
     <>
-      <BlogMainSection />
+      <IconContext.Provider value={{ size: "2.25em" }}>
+        <CursorContext.Provider value={{ textEnter, textLeave }}>
+          <HoverTypeContext.Provider value={{ hoverType, setHoverType }}>
+            <div>
+              <m.div
+                variants={variants}
+                animate={cursorVariant}
+                className="cursorr pointer-events-none fixed top-0 left-0 h-2 w-2 rounded-full bg-brand_bg-500 "
+              />
+              <m.div
+                variants={variants3}
+                animate={cursorVariant}
+                transition={{
+                  type: "spring",
+                  damping: 11,
+                  stiffness: 51,
+                  restDelta: 1.001,
+                }}
+                className="cursorr pointer-events-none absolute left-0 top-0 h-8 w-8 rounded-full border-[1px] border-brand_bg-500 "
+              />
+              <AnimatePresence>
+                <IconContext.Provider value={{ size: "1.5em" }}>
+                  <m.div
+                    variants={variants4}
+                    animate={cursorVariant}
+                    transition={{
+                      type: "spring",
+                      damping: 11,
+                      stiffness: 51,
+                      restDelta: 1.001,
+                    }}
+                    exit={{ opacity: 0 }}
+                    className={`${zen_tokyo_zoo.className} cursorrr pointer-events-none absolute left-0 top-0 hidden h-1 w-1 items-center justify-center rounded-3xl border-[1px] border-brand_bg-300  bg-brand_bg-300/90 font-bold text-[#333333] `}
+                  >
+                    {hoverType}
+                  </m.div>
+                </IconContext.Provider>
+              </AnimatePresence>
+            </div>
+            <BlogHeader />
+            <BlogMainSection />
+          </HoverTypeContext.Provider>
+        </CursorContext.Provider>
+      </IconContext.Provider>
     </>
   );
 };
 
-export default index;
+export default Blog;
