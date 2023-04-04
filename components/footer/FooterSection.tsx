53700756c62e3d727febd8180aaaac51ead2e38e
themuuln <zerone.offical@gmail.com>
2023-04-04 09:36:02 +0800

    revert to div

--- components/footer/FooterSection.tsx
+++ components/footer/FooterSection.tsx
@@ -4,14 +4,14 @@ import FooterMain from "./FooterMain";
 
 const FooterSection = () => {
   return (
-    <section id="footer" className="flex h-fit flex-col justify-end pt-20">
-      <footer className="text-brand_main-500 bg-brand_bg-600/5">
+    <div id="footer" className="flex h-fit flex-col justify-end pt-20">
+      <div className="text-brand_main-500 bg-brand_bg-600/5">
         <IconContext.Provider value={{ size: "1em" }}>
           {/* <FooterStartSection /> */}
           <FooterMain />
         </IconContext.Provider>
-      </footer>
-    </section>
+      </div>
+    </div>
   );
 };
 
--- components/header/HeaderSection.tsx
+++ components/header/HeaderSection.tsx
@@ -5,12 +5,12 @@ import HeaderRight from "./HeaderRight";
 const HeaderSection = () => {
   return (
     <>
-      <header className="sticky top-0 left-0 z-10 h-[80px] w-full py-6 px-8 text-white backdrop-blur-sm">
+      <div className="sticky top-0 left-0 z-10 h-[80px] w-full py-6 px-8 text-white backdrop-blur-sm">
         <div className="container flex justify-between ">
           <HeaderLeft context={"themuln.official@gmail.com"} />
           <HeaderRight />
         </div>
-      </header>
+      </div>
     </>
   );
 };
