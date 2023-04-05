395148249749f0c204a8ae0100b5343c1a66f88c
themuuln <zerone.offical@gmail.com>
2023-04-05 11:30:29 +0800

    update: view

--- components/header/HeaderSection.tsx
+++ components/header/HeaderSection.tsx
@@ -5,7 +5,7 @@ import HeaderRight from "./HeaderRight";
 const HeaderSection = () => {
   return (
     <>
-      <div className="sticky top-0 left-0 z-10 h-[80px] w-full py-6 px-8 text-white backdrop-blur-sm">
+      <div className="absolute top-0 z-10 h-[80px] w-full py-6 px-8 text-white backdrop-blur">
         <div className="container flex justify-between ">
           <HeaderLeft context={"themuln.official@gmail.com"} />
           <HeaderRight />
