e16eb40fda6af0fae37937c7ea00707cd98e2097
themuuln <zerone.offical@gmail.com>
2023-04-10 00:10:30 +0800

    tailwind config cleaned up

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,32 +4,23 @@ module.exports = {
     "./app/**/*.{js,ts,jsx,tsx}",
     "./pages/**/*.{js,ts,jsx,tsx}",
     "./components/**/*.{js,ts,jsx,tsx}",
-
-    // Or if using `src` directory:
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
     screens: {
       sm: "640px",
-      // => @media (min-width: 640px) { ... }
-
       md: "768px",
-      // => @media (min-width: 768px) { ... }
-
       lg: "1024px",
-      // => @media (min-width: 1024px) { ... }
-
       xl: "1280px",
-      // => @media (min-width: 1280px) { ... }
-
       "2xl": "1536px",
-      // => @media (min-width: 1536px) { ... }
     },
     container: {
       center: true,
     },
     extend: {
-      fontFamily: {},
+      fontFamily: {
+        firacode: ["var(--font-firacode)"],
+      },
       colors: {
         brand_bg: {
           100: "#e9d9fc",
