19dde35389e69ac979a4dde94f45e1f36968c27b
themuuln <zerone.offical@gmail.com>
2023-04-12 10:40:56 +0800

    animations updated

--- components/header/HeaderSection.tsx
+++ components/header/HeaderSection.tsx
@@ -10,15 +10,18 @@ const HeaderSection = () => {
     <>
       <div className="absolute top-0 z-10 h-[80px] w-full px-8 py-6 text-white backdrop-blur">
         <div className="container flex justify-between ">
-          <m.div
-            initial={{ x: 200, opacity: 0 }}
-            whileInView={{ x: 0, opacity: 1 }}
-            className="flex items-center justify-center gap-2 text-lg cursor-pointer leftSideHeader "
-            onMouseEnter={textEnter}
-            onMouseLeave={textLeave}
-          >
-            Temuulen Undrakhbayar
-          </m.div>
+          <div className="overflow-x-hidden overflow-y-hidden w-fit">
+            <m.div
+              initial={{ y: 100, opacity: 0.3 }}
+              animate={{ y: 0, opacity: 1 }}
+              transition={{ duration: 1 }}
+              className="flex items-center justify-center gap-2 text-lg cursor-pointer leftSideHeader "
+              onMouseEnter={textEnter}
+              onMouseLeave={textLeave}
+            >
+              Temuulen Undrakhbayar
+            </m.div>
+          </div>
           <HeaderRight />
         </div>
       </div>
