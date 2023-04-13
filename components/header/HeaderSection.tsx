bc7a4507c8eb180d11e30a5f328ae81d230da63c
themuuln <zerone.offical@gmail.com>
2023-04-13 18:21:41 +0800

    Headersection font updated

--- components/header/HeaderSection.tsx
+++ components/header/HeaderSection.tsx
@@ -3,6 +3,7 @@ import HeaderRight from "./HeaderRight";
 import { motion as m } from "framer-motion";
 import { useContext } from "react";
 import CursorContext from "@/lib/context/context";
+import { firacode } from "@/public/fonts/fonts";
 
 const HeaderSection = () => {
   const { textEnter, textLeave } = useContext(CursorContext);
@@ -15,7 +16,7 @@ const HeaderSection = () => {
               initial={{ y: 100, opacity: 0.3 }}
               animate={{ y: 0, opacity: 1 }}
               transition={{ duration: 1 }}
-              className="flex items-center justify-center gap-2 text-lg cursor-pointer leftSideHeader "
+              className={`${firacode.className} flex items-center justify-center gap-2 text-xl cursor-pointer leftSideHeader `}
               onMouseEnter={textEnter}
               onMouseLeave={textLeave}
             >
