fe3a11e2f5b63337dc6a26410e5bd51ea81e6f0e
themuuln <zerone.offical@gmail.com>
2023-04-14 01:40:01 +0800

    self closed

--- app/loading/page.tsx
+++ app/loading/page.tsx
@@ -2,7 +2,7 @@ const Loading = () => {
   return (
     <>
       <section className="container flex items-center justify-center min-h-screen min-w-screen">
-        <div className="dots"></div>
+        <div className="dots" />
       </section>
     </>
   );
