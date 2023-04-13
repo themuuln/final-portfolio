222dbf20c3fd7d3f8c3a4c47100aadfc79538d2b
themuuln <zerone.offical@gmail.com>
2023-04-14 01:01:27 +0800

    loader

---
+++ app/loading/page.tsx
@@ -0,0 +1,11 @@
+const Loading = () => {
+  return (
+    <>
+      <section className="container flex items-center justify-center min-h-screen min-w-screen">
+        <div className="dots"></div>
+      </section>
+    </>
+  );
+};
+
+export default Loading;
