9fdb2c901b6cf917e538864a625d50aef6492815
themuuln <zerone.offical@gmail.com>
2023-04-13 18:22:49 +0800

    typeError fixed

--- app/me/page.tsx
+++ app/me/page.tsx
@@ -81,7 +81,7 @@ const Themuuln = () => {
     <>
       <IconContext.Provider value={{ size: "2.25em" }}>
         <CursorContext.Provider value={{ textEnter, textLeave }}>
-          <HoverTypeContext.Provider value={{ hoverType, setHoverType }}>
+          <HoverTypeContext.Provider value={{ setHoverType }}>
             <div>
               <m.div
                 variants={variants}
