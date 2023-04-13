dd7e70573fc62e1a0a0a32715ce7ed89df9ed42e
themuuln <zerone.offical@gmail.com>
2023-04-13 19:27:13 +0800

    contact section added

--- components/contact/ContactHeader.tsx
+++ components/contact/ContactHeader.tsx
@@ -1,15 +1,31 @@
 "use client";
 import Link from "next/link";
 import Buttons from "../header/Buttons";
+import { motion as m } from "framer-motion";
 import { firacode } from "@/public/fonts/fonts";
+import { useContext } from "react";
+import CursorContext from "@/lib/context/context";
 import { HiOutlineAnnotation, HiOutlineHome } from "react-icons/hi";
 
 const ContactHeader = () => {
+  const { textEnter, textLeave } = useContext(CursorContext);
   return (
     <div
       className={`${firacode.className} sticky left-0 top-0 z-10 h-[80px] w-full px-8 py-6 text-white backdrop-blur-sm`}
     >
       <div className="container flex justify-between ">
+        <div className="overflow-x-hidden overflow-y-hidden w-fit">
+          <m.div
+            initial={{ y: 100, opacity: 0.3 }}
+            animate={{ y: 0, opacity: 1 }}
+            transition={{ duration: 1 }}
+            className={`${firacode.className} flex items-center justify-center gap-2 text-xl cursor-pointer leftSideHeader `}
+            onMouseEnter={textEnter}
+            onMouseLeave={textLeave}
+          >
+            Temuulen Undrakhbayar
+          </m.div>
+        </div>
         <ul className="flex gap-5 text-2xl font-bold ">
           <Link href={"#home"}>
             <Buttons icon={<HiOutlineHome />} context={"home"} />
--- components/contact/ContactSection.tsx
+++ components/contact/ContactSection.tsx
@@ -3,7 +3,38 @@ const ContactSection = () => {
     <>
       <section className="flex h-screen bg-brand_bg-600/5">
         <div className="container">
-          <input type="text" className="" />
+          <div className="flex justify-between gap-10">
+            <div className="relative flex flex-col">
+              <label className="text-sm leading-7 text-gray-600">
+                Your name
+              </label>
+              <input type="text" id="name" name="name" />
+            </div>
+            <div className="relative flex flex-col">
+              <label className="text-sm leading-7 text-gray-600">
+                Your email
+              </label>
+              <input type="text" id="name" name="name" />
+            </div>
+          </div>
+          <div className="flex justify-between gap-10">
+            <div className="relative flex flex-col">
+              <label className="text-sm leading-7 text-gray-600">
+                What you are interested
+              </label>
+              <input type="text" id="name" name="name" className="" />
+            </div>
+            <div className="relative flex flex-col">
+              <label className="text-sm leading-7 text-gray-600">
+                Project Budget
+              </label>
+              <input type="text" id="name" name="name" />
+            </div>
+          </div>
+          <div className="relative flex flex-col">
+            <label className="text-sm leading-7 text-gray-600">Message</label>
+            <input type="text" id="name" name="name" />
+          </div>
         </div>
       </section>
     </>
