d2284bb89672fe033ec9458e466cc4db1290856f
themuuln <zerone.offical@gmail.com>
2023-04-13 19:38:52 +0800

    skill section font size fixed

--- components/main/skills/SkillsText.tsx
+++ components/main/skills/SkillsText.tsx
@@ -19,7 +19,7 @@ const SkillsText = ({ name, href }: any) => {
       href={href}
       target="_blank"
       rel="noopener noreferrer"
-      className="text-lg font-light leading-3 sm:text-xl md:text-2xl md:leading-3 xl:text-3xl xl:leading-6"
+      className="text-base font-light leading-3 sm:text-lg md:text-xl md:leading-3 xl:text-2xl xl:leading-6"
     >
       {name}
     </m.a>
