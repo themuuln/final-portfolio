a132964eb4d8953c926a91ff17f03fef78c66bdf
themuuln <zerone.offical@gmail.com>
2023-04-13 18:21:08 +0800

    removed useless context state

--- lib/context/HoverTypeContext.tsx
+++ lib/context/HoverTypeContext.tsx
@@ -2,6 +2,5 @@ import { createContext } from "react";
 import { FiArrowUpRight } from "react-icons/fi";
 
 export const HoverTypeContext = createContext({
-  hoverType: <FiArrowUpRight />,
   setHoverType: (hoverType: any) => {},
 });
