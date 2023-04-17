c6a72d1ab452b569b3b3beb13084909ce5c265eb
themuuln <zerone.offical@gmail.com>
2023-04-17 17:37:02 +0800

    Loading Screen Timer set

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,27 @@
 "use client";
 const HeroSection = require("@/components/hero/HeroSection").default;
 const MainSection = require("@/components/main/MainSection").default;
+import { useEffect, useState } from "react";
+import Loading from "./loading";
 
 export default function Home() {
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      setLoading(false);
+    }, 3000);
+
+    return () => {
+      clearTimeout(timer);
+    };
+  }, []);
+
   return (
     <>
       <HeroSection />
       <MainSection />
+      {loading && <Loading />}
     </>
   );
 }
