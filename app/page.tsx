818899e7efb0489809033b57b3bd7015d8af6ae0
themuuln <zerone.offical@gmail.com>
2023-04-17 17:45:16 +0800

    Timer removed from page

--- app/page.tsx
+++ app/page.tsx
@@ -1,32 +1,12 @@
 "use client";
 const HeroSection = require("@/components/hero/HeroSection").default;
 const MainSection = require("@/components/main/MainSection").default;
-import { useEffect, useState } from "react";
-import Loading from "./loading";
 
 export default function Home() {
-  const [loading, setLoading] = useState(true);
-
-  useEffect(() => {
-    const timer = setTimeout(() => {
-      setLoading(false);
-    }, 3000);
-
-    return () => {
-      clearTimeout(timer);
-    };
-  }, []);
-
   return (
-    <div>
-      {loading ? (
-        <Loading />
-      ) : (
-        <>
-          <HeroSection />
-          <MainSection />
-        </>
-      )}
-    </div>
+    <>
+      <HeroSection />
+      <MainSection />
+    </>
   );
 }
