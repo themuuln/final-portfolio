3362c2a75cb7321bac8dfde271eba094039c536a
themuuln <zerone.offical@gmail.com>
2023-04-18 20:34:45 +0800

    Fixed padding bug, updated color

--- components/main/Mbti/ChartRight.tsx
+++ components/main/Mbti/ChartRight.tsx
@@ -16,11 +16,11 @@ const ChartRight = ({ chartColor, shadowColor }: Props) => {
         whileInView={chart.whileInView}
         whileHover={chart.whileHover}
         transition={{ type: "tween" }}
-        className={`relative h-10 w-[300px] rounded-[30px] bg-chart_bg-600 p-1 md:w-[400px] xl:w-[600px]`}
+        className={`relative h-10 w-[300px] rounded-[30px] bg-light_bg-600/50 dark:bg-chart_bg-600 p-1 md:w-[400px] xl:w-[600px]`}
       >
         <m.div
           id={`${shadowColor}`}
-          className={`h-8 ${shadowColor}Glow absolute right-0 rounded-[30px] rounded-l-none ${chartColor}`}
+          className={`h-8 ${shadowColor}Glow absolute right-1 rounded-[30px] rounded-l-none ${chartColor}`}
         />
       </m.div>
     </>
