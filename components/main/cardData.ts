36e7277e19708c8f31e24bfa3eeb2f9825e703cf
themuuln <zerone.offical@gmail.com>
2023-04-18 21:04:49 +0800

    Mistype fixed

--- components/main/cardData.ts
+++ components/main/cardData.ts
@@ -1,18 +1,18 @@
 export const cardData = [
   {
-    title: "WHAT I DO?",
+    title: "WHAT I ",
     highlightedText: "DO?",
     description:
       "I create customized websites that are visually appealing, highly functional, and geared towards boosting your online profile, branding, and revenue. I leverage the latest web technologies to provide tailored digital solutions that cater to your specific requirements and objectives.",
   },
   {
-    title: "WHY CHOOSE ME?",
+    title: "WHY CHOOSE ",
     highlightedText: "ME?",
     description:
       "I specialize in crafting bespoke, visually stunning websites that function seamlessly to enhance your online presence, brand identity, and revenue streams. With my expertise in the latest web technologies, I provide customized digital solutions that cater to your unique needs and objectives.",
   },
   {
-    title: "PHILOSOPHY OF MINE",
+    title: "PHILOSOPHY OF ",
     highlightedText: "MINE",
     description:
       "I create stunning bespoke websites that enhance your online presence, brand identity, and revenue streams. Using the latest web technologies, I provide tailored digital solutions to meet your unique needs and objectives.",
