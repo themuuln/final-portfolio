68f6c36859c34d59bff89d791ebe2f91158a6b6b
themuuln <zerone.offical@gmail.com>
2023-04-20 22:47:34 +0800

    Project section added

--- components/main/MainSection.tsx
+++ components/main/MainSection.tsx
@@ -7,6 +7,7 @@ import SeeMoreSection from "./SeeMoreSection";
 import TimelineSection from "./TimelineSection";
 import dynamic from "next/dynamic";
 import { useMediaQuery } from "react-responsive";
+import ProjectsSection from "./ProjectsSection";
 const VerticalLine = dynamic(() => import("../VerticalLine"), { ssr: false });
 
 const MainSection = () => {
@@ -53,6 +54,7 @@ const MainSection = () => {
         </div>
       </section>
       <SkillSection />
+      <ProjectsSection />
       <TimelineSection />
       <SeeMoreSection />
     </>
---
+++ components/main/ProjectsSection.tsx
@@ -0,0 +1,11 @@
+const ProjectsSection = () => {
+  return (
+    <>
+      <div className="flex items-center justify-center w-full h-screen">
+        ProjectSection
+      </div>
+    </>
+  );
+};
+
+export default ProjectsSection;
