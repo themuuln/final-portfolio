6f2656619fbaa6f8dfdbefd18da0d003a063d12b
themuuln <zerone.offical@gmail.com>
2023-04-20 16:14:02 +0800

    Updated line, text color

--- components/VerticalLine.tsx
+++ components/VerticalLine.tsx
@@ -6,9 +6,9 @@ type VerticalLineProps = {
 const VerticalLine = ({ element }: VerticalLineProps) => {
   return (
     <>
-      <div className="space-y-2 h-[115%] font-normal pointer-events-none px-2 -mt-[2%]">
+      <div className="space-y-2 h-[115%] font-normal dark:text-white/30 text-black/30 pointer-events-none px-2 -mt-[2%]">
         <div className="">&lt;{element}&gt;</div>
-        <m.div className="relative w-[1px] ml-[50%] h-[90%] bg-gray-800/40"></m.div>
+        <m.div className="relative w-[1px] ml-[50%] h-[90%] dark:bg-white/10 bg-black/10"></m.div>
         <div className="">&lt;/{element}&gt;</div>
       </div>
     </>
