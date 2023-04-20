e128397734e421b668893a03878aa8647feae687
themuuln <zerone.offical@gmail.com>
2023-04-20 15:59:02 +0800

    Accordtion DAta Template

---
+++ pages/api/accordions.ts
@@ -0,0 +1,26 @@
+export const accordions = [
+  {
+    id: 1,
+    question: "How can I book a workout class?",
+    answer:
+      "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Molestiae temporibus beatae, totam repudiandae nam recusandae ea dolores tempora maxime. adipisicing elit. Molestiae temporibus beatae, totam repudiandae adipisicing elit. Molestiae temporibus beatae, totam repudiandae nam",
+  },
+  {
+    id: 2,
+    question: "Can I pay by cash for my membership?",
+    answer:
+      "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Molestiae temporibus beatae, totam repudiandae nam recusandae ea dolores tempora maxime.olestiae temporibus beatae, totam repudiandae nam recusandae ea dolores tempora maxime.  adipisicing elit. Molestiae temporibus beatae, totam repudiandae",
+  },
+  {
+    id: 3,
+    question: "What age do I need to be to join?",
+    answer:
+      "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Molestiae temporibus beatae, totam repudiandae nam recusandae ea dolores tempora maxime.totam repudiandae nam recusandae ea dolores tempora maxime.  adipisicing elit. Molestiae temporibus beatae it amet consectetur, adipisicing elit. Molestiae temporibus beatae, totam repudiandae nam recusandae ea dolores tempora maxime.totam repudiandae nam recusandae ea dolores",
+  },
+  {
+    id: 4,
+    question: "Are there any lockers?",
+    answer:
+      "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Molestiae temporibus beatae, totam repudiandae nam recusandae ea dolores tempora maxime. dolor sit amet  dolor sit amet ",
+  },
+];
