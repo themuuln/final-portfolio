5955f3d6776e68eecb3da369e6a83ddd1286b8c3
themuuln <zerone.offical@gmail.com>
2023-04-24 10:19:02 +0800

    Project banner added

---
+++ components/main/Projects/ProjectBanner.tsx
@@ -0,0 +1,9 @@
+const ProjectBanner = ({ children }: any) => {
+  return (
+    <div className="w-full h-40 bg-dark_bg-500/50 border-b-[1px] border-white">
+      {children}
+    </div>
+  );
+};
+
+export default ProjectBanner;
