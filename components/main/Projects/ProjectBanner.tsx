66674324dccbb697c3119d70575ac516b69f504e
themuuln <zerone.offical@gmail.com>
2023-05-24 00:48:02 +0800

    Projects Section Added

--- components/main/Projects/ProjectBanner.tsx
+++ components/main/Projects/ProjectBanner.tsx
@@ -1,8 +1,13 @@
-const ProjectBanner = ({ children }: any) => {
+import Link from "next/link";
+
+const ProjectBanner = ({ name, href }: any) => {
   return (
-    <div className="w-full h-40 bg-dark_bg-500/50 border-b-[1px] border-white">
-      {children}
-    </div>
+    <Link
+      href={href}
+      className="w-full flex items-center justify-center text-2xl h-40 bg-dark_bg-500/50 border-b-[1px] border-white"
+    >
+      {name}
+    </Link>
   );
 };
 
---
+++ components/main/Projects/ProjectsData.ts
@@ -0,0 +1,28 @@
+const ProjectsData = [
+  {
+    name: "Project1",
+    href: "/project/1",
+  },
+  {
+    name: "Project2",
+    href: "/project/2",
+  },
+  {
+    name: "Project3",
+    href: "/project/3",
+  },
+  {
+    name: "Project4",
+    href: "/project/4",
+  },
+  {
+    name: "Project5",
+    href: "/project/5",
+  },
+  {
+    name: "Project6",
+    href: "/project/6",
+  },
+];
+
+export default ProjectsData;
