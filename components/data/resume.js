a12d1573b461b5ca49a1e2e0f9ee53236f7cc3b6
themuuln <zerone.offical@gmail.com>
2023-05-24 00:44:41 +0800

    ResumeData Updated

---
+++ components/data/resume.js
@@ -0,0 +1,49 @@
+const resumeData = [
+  {
+    technologies: [
+      "HTML5",
+      "CSS3",
+      "JavaScript (ES6+)",
+      "TypeScript",
+      "React",
+      "NextJS",
+      "TailwindCSS",
+      "Chakra-UI",
+      "SASS/SCSS",
+      "Styled-components",
+      "Framer Motion",
+      "Git",
+      "Figma",
+      "Neovim",
+      "Vercel",
+      "Firebase",
+    ],
+    links: [
+      { href: "https://themuuln.vercel.app", name: "Portfolio" },
+      { href: "http://github.com/themuuln", name: "Github" },
+      { href: "https://www.linkedin.com/in/themuuln/", name: "LinkedIn" },
+    ],
+    professionalSummary:
+      "Organized and dependable candidate successful at managing multiple priorities with a positive attitude. Willingness to take on added responsibilities to meet team goals. Hardworking and passionate job seeker with strong organizational skills eager to secure entry-level Front-end Developer position. Ready to help team achieve company goals.",
+    professionalExperience: {
+      title: "Front-end Developer Intern",
+      company: "Mongolian National Lottery",
+      location: "Ulaanbaatar, Mongolia",
+      responsibilities: [
+        "Skilled at working independently and collaboratively in a team environment.",
+        "Self-motivated, with a strong sense of personal responsibility.",
+        "Excellent communication skills, both verbal and written.",
+        "Proven ability to learn quickly and adapt to new situations.",
+        "Worked well in a team setting, providing support and guidance.",
+        "Demonstrated respect, friendliness, and willingness to help wherever needed.",
+      ],
+    },
+    education: ["Erdmiin Urgoo - Bayangol, Ulaanbaatar 06/2022"],
+    languages: [
+      { name: "Mongolian", proficiency: "Native language" },
+      { name: "English", proficiency: "B2 (Upper intermediate)" },
+    ],
+  },
+];
+
+export default resumeData;
