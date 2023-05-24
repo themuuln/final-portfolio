98f94300094303411b1aa20b8ab4670ae91bb13d
themuuln <zerone.offical@gmail.com>
2023-05-24 17:00:23 +0800

    Cleaned

--- app/resume/page.tsx
+++ app/resume/page.tsx
@@ -6,9 +6,19 @@ import resumeData from "@/components/data/resume";
 import CursorContext from "@/lib/context/context";
 import { useContext } from "react";
 import { HoverTypeContext } from "@/lib/context/HoverTypeContext";
+import { FiArrowUpRight } from "react-icons/fi";
+import { IconContext } from "react-icons";
 
 const ResumeTitle = ({ children }: { children: string }) => {
-  return <h1 className="text-2xl font-semibold md:text-3xl">{children}</h1>;
+  return (
+    <h1 className="text-2xl font-semibold text-brand_bg-500 md:text-3xl">
+      {children}
+    </h1>
+  );
+};
+
+const SectionContainer = ({ children }: any) => {
+  return <div className="mb-4">{children}</div>;
 };
 
 const Resume = () => {
@@ -24,17 +34,19 @@ const Resume = () => {
     <section className="flex justify-center">
       <div className="container px-4 mt-20">
         <div>
-          <h1 className="text-3xl font-bold md:text-6xl">
+          <h1 className="text-3xl font-bold text-brand_bg-500 md:text-6xl">
             TEMUULEN UNDRAKHBAYAR
           </h1>
-          <div className="mb-4">
+
+          <SectionContainer>
             <p>
               themuln.official@gmail.com | <span>+976 88650115</span>
             </p>
             <p>Ulaanbaatar, Mongolia</p>
-          </div>
+          </SectionContainer>
         </div>
-        <div className="mb-4">
+
+        <SectionContainer>
           <ResumeTitle>PORTFOLIOS, PROFILES</ResumeTitle>
           <ul>
             {resumeData[0].links.map((link, index) => (
@@ -42,48 +54,58 @@ const Resume = () => {
                 key={index}
                 onMouseEnter={handleMouseEnter}
                 onMouseLeave={textLeave}
+                className="flex items-center gap-[2px] hover:text-brand_bg-500 transition-colors duration-300"
               >
-                <Links href={link.href} name={link.name} />
+                <IconContext.Provider value={{ size: "1.25em" }}>
+                  <Links href={link.href} name={link.name} /> <FiArrowUpRight />
+                </IconContext.Provider>
               </motion.li>
             ))}
           </ul>
-        </div>
-        <div>
+        </SectionContainer>
+
+        <SectionContainer>
           <ResumeTitle>PROFESSIONAL SUMMARY</ResumeTitle>
           <p>{resumeData[0].professionalSummary}</p>
-        </div>
-        <div>
+        </SectionContainer>
+
+        <SectionContainer>
           <ResumeTitle>SKILLS</ResumeTitle>
           <div className="grid grid-cols-2 text-xl">
             {resumeData[0].technologies.map((technology) => (
               <li key={technology}>{technology}</li>
             ))}
           </div>
-        </div>
+        </SectionContainer>
 
-        <div>
+        <SectionContainer>
           <ResumeTitle>PROFESSIONAL EXPERIENCE</ResumeTitle>
           <p>
-            <span>{resumeData[0].professionalExperience.title}</span> |{" "}
-            {resumeData[0].professionalExperience.company} -{" "}
-            {resumeData[0].professionalExperience.location}
+            <span className="font-bold">
+              {resumeData[0].professionalExperience.title}
+            </span>{" "}
+            | {resumeData[0].professionalExperience.company} -{" "}
+            {resumeData[0].professionalExperience.location}{" "}
+            {resumeData[0].professionalExperience.date}
           </p>
           <ul>
             {resumeData[0].professionalExperience.responsibilities.map(
               (responsibility, index) => (
-                <li key={index}>{responsibility}</li>
+                <li key={index}>- {responsibility}</li>
               )
             )}
           </ul>
-        </div>
-        <div className="">
+        </SectionContainer>
+
+        <SectionContainer>
           <ResumeTitle>Education</ResumeTitle>
           <div className="flex justify-between w-full">
             <p>{resumeData[0].education[0]}</p>
             <p>06/2022</p>
           </div>
-        </div>
-        <div className="">
+        </SectionContainer>
+
+        <SectionContainer>
           <ResumeTitle>Languages</ResumeTitle>
           {resumeData[0].languages.map((language, index) => (
             <div key={index}>
@@ -103,7 +125,7 @@ const Resume = () => {
               )}
             </div>
           ))}
-        </div>
+        </SectionContainer>
       </div>
     </section>
   );
