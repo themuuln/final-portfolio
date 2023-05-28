5857c2fb0c71a28929bc4fc874b5c3a39e37e625
themuuln <zerone.offical@gmail.com>
2023-05-29 03:53:47 +0800

    Header condition adjusted

--- components/header/HeaderDesktop.tsx
+++ components/header/HeaderDesktop.tsx
@@ -4,6 +4,7 @@ import { motion as m } from "framer-motion";
 import ThemeSwitcher from "./ThemeSwitcher";
 import { IconContext } from "react-icons";
 import ButtonData from "./ButtonData";
+import { useMediaQuery } from "react-responsive";
 
 const HeaderDesktop = () => {
   const container = {
@@ -11,24 +12,26 @@ const HeaderDesktop = () => {
     show: { opacity: 1, transition: { staggerChildren: 0.5 } },
   };
   const buttons = { hidden: { y: "100%" }, show: { y: 0 } };
-
+  const isPortrait = useMediaQuery({ query: "(orientation: portrait)" }); // Check if the current screen orientation is portrait
   return (
     <>
       <m.ul
         variants={container} // Apply container variants for animation
         initial={"hidden"} // Set initial animation state
         animate={"show"} // Set target animation state
-        className="flex items-center gap-5 text-2xl font-bold headerDesktop "
+        className={`flex items-center gap-5 text-2xl font-bold headerDesktop`}
       >
-        {ButtonData.map((link, index) => (
-          <Link key={index} href={link.href}>
-            <Buttons
-              variants={buttons} // Pass buttons variants to Buttons component
-              icon={link.icon} // Pass icon prop to Buttons component
-              context={link.context} // Pass context prop to Buttons component
-            />
-          </Link>
-        ))}
+        <div className={`${isPortrait ? "hidden" : null} flex gap-5`}>
+          {ButtonData.map((link, index) => (
+            <Link key={index} href={link.href}>
+              <Buttons
+                variants={buttons} // Pass buttons variants to Buttons component
+                icon={link.icon} // Pass icon prop to Buttons component
+                context={link.context} // Pass context prop to Buttons component
+              />
+            </Link>
+          ))}
+        </div>
         <IconContext.Provider value={{ size: "0.5em" }}>
           <ThemeSwitcher />
         </IconContext.Provider>
--- components/header/HeaderRight.tsx
+++ components/header/HeaderRight.tsx
@@ -1,12 +1,43 @@
-import dynamic from "next/dynamic";
+import Link from "next/link";
+import Buttons from "./Buttons";
+import { motion as m } from "framer-motion";
+import ThemeSwitcher from "./ThemeSwitcher";
+import { IconContext } from "react-icons";
+import ButtonData from "./ButtonData";
 import { useMediaQuery } from "react-responsive";
 
-const HeaderMobile = dynamic(() => import("./HeaderMobile"), { ssr: false }); // Dynamically import HeaderMobile component with SSR (Server-Side Rendering) disabled
-const HeaderDesktop = dynamic(() => import("./HeaderDesktop"), { ssr: false }); // Dynamically import HeaderDesktop component with SSR (Server-Side Rendering) disabled
-
 const HeaderRight = () => {
+  const container = {
+    hidden: { opacity: 0 },
+    show: { opacity: 1, transition: { staggerChildren: 0.5 } },
+  };
+  const buttons = { hidden: { y: "100%" }, show: { y: 0 } };
   const isPortrait = useMediaQuery({ query: "(orientation: portrait)" }); // Check if the current screen orientation is portrait
-  return isPortrait ? <HeaderMobile /> : <HeaderDesktop />; // Render HeaderMobile or HeaderDesktop component based on screen orientation
+  return (
+    <>
+      <m.ul
+        variants={container} // Apply container variants for animation
+        initial={"hidden"} // Set initial animation state
+        animate={"show"} // Set target animation state
+        className={`flex items-center gap-5 text-2xl font-bold headerDesktop`}
+      >
+        <div className={`${isPortrait ? "hidden" : null} flex gap-5`}>
+          {ButtonData.map((link, index) => (
+            <Link key={index} href={link.href}>
+              <Buttons
+                variants={buttons} // Pass buttons variants to Buttons component
+                icon={link.icon} // Pass icon prop to Buttons component
+                context={link.context} // Pass context prop to Buttons component
+              />
+            </Link>
+          ))}
+        </div>
+        <IconContext.Provider value={{ size: "0.5em" }}>
+          <ThemeSwitcher />
+        </IconContext.Provider>
+      </m.ul>
+    </>
+  );
 };
 
 export default HeaderRight;
