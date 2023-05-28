8ba55b5fda47566e5abfc718b09d0f8ade9e0f84
themuuln <zerone.offical@gmail.com>
2023-05-29 04:05:30 +0800

    Loading screen updated

--- app/loading.tsx
+++ app/loading.tsx
@@ -3,7 +3,7 @@ const Loading = () => {
     <>
       <section className="flex items-center justify-center min-h-screen noise-container min-w-screen">
         <div className="noise"></div>
-        <div className="dots"></div>
+        <div className="progress"></div>
       </section>
     </>
   );
