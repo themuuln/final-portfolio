59e69acede42387626d68c76e1b90e3baadd6515
themuuln <zerone.offical@gmail.com>
2023-05-30 01:54:41 +0800

    card data updated

--- components/main/cardData.ts
+++ components/main/cardData.ts
@@ -1,20 +1,20 @@
 export const cardData = [
   {
-    title: "WHAT I ",
-    highlightedText: "DO?",
+    title: "QUICK ",
+    highlightedText: "INTRODUCTION",
     description:
-      "I create customized websites that are visually appealing, highly functional, and geared towards boosting your online profile, branding, and revenue. I leverage the latest web technologies to provide tailored digital solutions that cater to your specific requirements and objectives.",
+      "Welcome to my digital realm! I'm Temuulen, a self-taught front-end enthusiast dedicated to crafting exceptional web experiences. With a blend of innovation and caffeine-fueled coding sessions, I transform lines of code into captivating stories. Join me on a thrilling journey through modern web development!",
   },
   {
-    title: "WHY CHOOSE ",
+    title: "ABOUT ",
     highlightedText: "ME?",
     description:
-      "I specialize in crafting bespoke, visually stunning websites that function seamlessly to enhance your online presence, brand identity, and revenue streams. With my expertise in the latest web technologies, I provide customized digital solutions that cater to your unique needs and objectives.",
+      "I'm a curious explorer who fell in love with front-end development. With an unconventional path and a multitude of online resources, I immersed myself in the ever-evolving landscape of web design. I thrive on pushing boundaries, thinking disruptively, and creating digital experiences that leave an indelible mark.",
   },
   {
-    title: "PHILOSOPHY OF ",
-    highlightedText: "MINE",
+    title: "MY ",
+    highlightedText: "SKILLS",
     description:
-      "I create stunning bespoke websites that enhance your online presence, brand identity, and revenue streams. Using the latest web technologies, I provide tailored digital solutions to meet your unique needs and objectives.",
+      "Within my toolkit, you'll find an array of cutting-edge front-end wizardry. From crafting sleek, responsive layouts to conjuring immersive animations that dance across the screen, I relish the challenge of transforming ideas into flawless digital realities. With an unwavering eye for design and an ardent passion for seamless user experiences, I strive to transport visitors into a realm of awe and inspiration.",
   },
 ];
