42190b2ddb5da20f2b6878caac5638b38ac4aa45
themuuln <zerone.offical@gmail.com>
2023-05-30 01:55:45 +0800

    section added

--- app/page.tsx
+++ app/page.tsx
@@ -5,6 +5,7 @@ import ContactSection from "@/components/contact/ContactSection";
 import SkillSection from "@/components/main/SkillSection";
 import TimelineSection from "@/components/main/TimelineSection";
 import SeeMoreSection from "@/components/main/SeeMoreSection";
+import MadeWith from "@/components/madewith/MadeWith";
 
 export default function Home() {
   return (
@@ -13,12 +14,13 @@ export default function Home() {
         <div className="noise" />
         <HeroSection />
         <div className="space-y-72 bodydivLight dark:bodydivDark ">
-          <div className="absolute w-full h-96 bg-gradient-to-b from-[#e6e6e6] via-[#e6e6e6] dark:from-[#171920]/80 dark:via-[#171920]/80" />
+          <div className="absolute w-full h-96 bg-gradient-to-b from-[#e6e6e6] via-[#e6e6e6] dark:from-[#16181f]/80 dark:via-[#16181f]/80" />
           <MainSection />
           <SkillSection />
           <TimelineSection />
           <SeeMoreSection />
           <ContactSection />
+          <MadeWith />
           <div className="absolute bottom-0 w-full h-96 bg-gradient-to-t from-[#e6e6e6] via-[#e6e6e6] dark:from-[#171920]/80 dark:via-[#171920]/80" />
         </div>
       </div>
