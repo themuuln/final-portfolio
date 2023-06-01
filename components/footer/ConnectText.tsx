6522680ea809d3f2888fd9d04797ca2139571763
themuuln <zerone.offical@gmail.com>
2023-06-01 17:00:46 +0800

    removed

--- components/footer/ConnectText.tsx
+++ components/footer/ConnectText.tsx
@@ -3,7 +3,6 @@ import { useContext } from "react";
 import CursorContext from "@/lib/context/context";
 import { motion as m } from "framer-motion";
 import { HoverTypeContext } from "@/lib/context/HoverTypeContext";
-import { FiArrowUpRight } from "react-icons/fi";
 
 const ConnectText = ({ href, social, icon }: any) => {
   const { textEnter, textLeave } = useContext(CursorContext);
@@ -14,16 +13,12 @@ const ConnectText = ({ href, social, icon }: any) => {
     setHoverType(icon);
   };
 
-  const handleMouseLeave = () => {
-    textLeave();
-    setHoverType(<FiArrowUpRight />);
-  };
   return (
     <li>
       <m.a
         href={href}
         onMouseEnter={handleMouseEnter}
-        onMouseLeave={handleMouseLeave}
+        onMouseLeave={textLeave}
         className="flex items-center justify-center gap-1 transition-all duration-500 hover:text-brand_main-700 underline-offset-1 hover:underline"
         target={"blank"}
       >
