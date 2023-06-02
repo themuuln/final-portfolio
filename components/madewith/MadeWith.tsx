adac51e1208532e4a9376230b5d83af5a95ffa1b
themuuln <zerone.offical@gmail.com>
2023-06-02 18:18:59 +0800

    Madewith animation added

--- components/madewith/MadeWith.tsx
+++ components/madewith/MadeWith.tsx
@@ -12,6 +12,7 @@ import yarnIcon from "public/svg/yarn.svg";
 import reactIconsIcon from "public/svg/react-icons.svg";
 import sassIcon from "public/svg/sass.svg";
 import flowbiteIcon from "public/svg/flowbite.svg";
+import { motion as m } from "framer-motion";
 // import axiosIcon from "public/svg/axios.svg";
 
 const icons = [
@@ -31,27 +32,69 @@ const icons = [
   yarnIcon,
 ];
 
+const container = {
+  hidden: {
+    opacity: 0,
+    y: -30,
+  },
+  show: {
+    opacity: 1,
+    y: 0,
+    transition: {
+      ease: "easeInOut",
+      staggerChildren: 0.2,
+    },
+  },
+};
+
+const item = {
+  hidden: {
+    opacity: 0,
+    y: "100%",
+  },
+  show: {
+    opacity: 1,
+    y: 0,
+    transition: {
+      ease: "easeInOut",
+    },
+  },
+};
+
 const MadeWith = () => {
   return (
     <div className="pb-72">
       <div className="sm:px-6 lg:px-8 container px-4 mx-auto">
-        <div className="mt-8 select-none pointer-events-none relative shadow-lg bg-slate-100 backdrop-blur-[2px] dark:bg-background py-8 px-6 sm:py-10 sm:px-8 md:py-12 md:px-10 lg:py-16 lg:px-12">
-          <p className="mb-4 text-lg text-center">Made this website with</p>
+        <m.div
+          variants={container}
+          initial="hidden"
+          whileInView="show"
+          transition={{ ease: "easeInOut", delay: 0.5 }}
+          className="mt-8 select-none pointer-events-none relative shadow-lg bg-slate-100 backdrop-blur-[2px] dark:bg-background py-8 px-6 sm:py-10 sm:px-8 md:py-12 md:px-10 lg:py-16 lg:px-12"
+        >
+          <p className="md:text-2xl md:mb-8 mb-4 text-lg text-center">
+            Made this website with
+          </p>
           <div className="flex flex-wrap justify-center gap-4">
             {icons.map((icon, index) => (
               <div
                 key={index}
                 className="sm:w-1/2 md:w-1/3 lg:w-1/4 xl:w-1/6 sm:mb-0 w-full mb-4 text-center"
               >
-                <Image
-                  className="sm:h-15 z-50 h-12 mx-auto mb-2"
-                  src={icon}
-                  alt=""
-                />
+                <m.div
+                  variants={item}
+                  className="sm:h-20 z-50 h-12 mx-auto mb-2"
+                >
+                  <Image
+                    className="sm:h-20 z-50 h-12 mx-auto mb-2"
+                    src={icon}
+                    alt=""
+                  />
+                </m.div>
               </div>
             ))}
           </div>
-        </div>
+        </m.div>
       </div>
     </div>
   );
