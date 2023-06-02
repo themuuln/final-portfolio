a59bf660bf358997fca1fe1d8fd810eb9612f801
themuuln <zerone.offical@gmail.com>
2023-06-03 02:34:41 +0800

    Comments removed

--- components/header/Buttons.tsx
+++ components/header/Buttons.tsx
@@ -1,41 +1,38 @@
-import { motion, useTransform, useScroll } from "framer-motion";
+import { motion } from "framer-motion";
 import CursorContext from "@/lib/context/context";
 import { useContext } from "react";
 import { HoverTypeContext } from "@/lib/context/HoverTypeContext";
 import { useMediaQuery } from "react-responsive";
 
 const Buttons = ({
-  context, // Prop for button context
-  icon, // Prop for button icon
-  variants, // Prop for animation variants
+  context,
+  icon,
+  variants,
 }: {
   context: string;
   icon: any;
   variants?: any;
 }) => {
-  const { textEnter, textLeave } = useContext(CursorContext); // Use CursorContext for textEnter and textLeave functions
-  const { setHoverType } = useContext(HoverTypeContext); // Use HoverTypeContext for setHoverType function
-  const isPortrait = useMediaQuery({ query: "(orientation: portrait)" }); // Check if the current screen orientation is portrait
+  const { textEnter, textLeave } = useContext(CursorContext);
+  const { setHoverType } = useContext(HoverTypeContext);
+  const isPortrait = useMediaQuery({ query: "(orientation: portrait)" });
 
   const handleMouseEnter = () => {
-    textEnter(); // Call textEnter function on mouse enter
-    setHoverType(icon); // Call setHoverType function with icon prop as argument
+    textEnter();
+    setHoverType(icon);
   };
 
   return (
     <motion.div className="w-fit overflow-hidden">
       <motion.li
-        onMouseEnter={handleMouseEnter} // Attach handleMouseEnter function to onMouseEnter event
-        onMouseLeave={textLeave} // Attach handleMouseLeave function to onMouseLeave event
-        variants={variants} // Apply variants prop for animation
-        initial={"hidden"} // Set initial animation state
-        animate={"show"} // Set target animation state
-        transition={{ duration: 0.5 }} // Set animation duration
+        onMouseEnter={handleMouseEnter}
+        onMouseLeave={textLeave}
+        variants={variants}
         className={`${
           isPortrait ? "hidden" : null
         }  header-button cursor-pointer font-light transition ease-linear`}
       >
-        &#47;&#47; {context} {/* Render button context */}
+        &#47;&#47; {context}
       </motion.li>
     </motion.div>
   );
